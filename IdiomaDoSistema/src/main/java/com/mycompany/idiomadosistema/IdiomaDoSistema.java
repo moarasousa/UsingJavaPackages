e1b8ea40fa82f19395d901161060c1d59670a108
Moara Sousa <moara.sousa15@outlook.com>
2023-10-20 23:35:11 -0300

    IdiomaDoSistema

---
+++ IdiomaDoSistema/src/main/java/com/mycompany/idiomadosistema/IdiomaDoSistema.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.idiomadosistema;
+
+import java.util.Locale;
+
+public class IdiomaDoSistema {
+
+    public static void main(String[] args) {
+        Locale local = Locale.getDefault();
+        System.out.println("O idioma do sistema é:");
+        System.out.println(local.toString());
+    }
+}
